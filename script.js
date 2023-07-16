b794fb76a0b4397329c0c1cbc914a32b775fd6d4
Orhan Kacar <orhankacar@web.de>
2023-07-16 14:36:12 +0200

    did the images and containers bigger

--- script.js
+++ script.js
@@ -1,44 +1,57 @@
-let pushedPokemons = [];
+let allPokemon = [];
+let url ='https://pokeapi.co/api/v2/pokemon/'
+let numberOfPokemon = 30
+
 
 async function loadPokemon() {
-  let url = `https://pokeapi.co/api/v2/pokemon?offset=0&limit=51`;
-  let response = await fetch(url);
-  let amount = await response.json();
+    for (let i = 0; i < numberOfPokemon; i++) {
+        const pokemon_url = url + (i + 1);
+        let response = await fetch(pokemon_url);
+        currentPokemon = await response.json();
+        allPokemon.push(currentPokemon);
+        renderPokemonInfoMain(i);
+        setTimeout(() => {
+            document.getElementById(`load-sccren`).classList.add(`d-none`);
+        }, "1600")
 
-  console.log("Hier sind die Anzahl Resultate", amount);
-
-  renderPokemonAmount(amount);
+    }
 }
 
-// renders the items with each url and pushes it to array with 50 pokemons
-async function renderPokemonAmount(amount) {
-  for (let i = 0; i < amount.results.length; i++) {
-    const pokemon = await loadPokemonUrl(amount["results"][i]["url"]);
-    pushedPokemons.push(pokemon);
-    renderPokemon(pokemon)
-  }
+function renderPokemonInfoMain(i) {
+    let name = allPokemon[i][`name`];
+    let types = allPokemon[i][`types`][0][`type`][`name`];
+    let img = allPokemon[i][`sprites`][`other`][`dream_world`][`front_default`];
+    let mainContainer = document.getElementById('pokedex');
+    mainContainer.innerHTML += '';
+    mainContainer.innerHTML += generateMainHtml(i, name, types, img);
+    bgColor(i);
 }
 
 
-function renderPokemon(pokemon) {
-    const pokemonName = pokemon['forms'][0]['name']
-    const pokemonImage = pokemon ['sprites']['other']['dream_world']['front_default']
-    const pokemonType = pokemon['types'][0]['type']['name']
-    const pokemonID =  pokemon['id']
+function generateMainHtml(i, name, types, img) {
+    return `
+     <div onclick="renderDialog(${i})"  class="container-main" id="background-color-main(${i})">
+     <div class="main-info-container">
 
-     document.getElementById('pokemon-card').innerHTML += `
-
-        <div class="pokemon-cards" onclick="openPokemonHTML(${pokemon.id})">
-            <div>
-                <div class="pokemon-id"><h2>#${pokemonID}</h2></div>
-                <img id="pokemon-image" class="pokemon-image" src="${pokemonImage}">
-                <h3 id="pokemon-name">${pokemonName}</h3>
-                <div id="types${pokemon.id}"></div>
-            </div>
-        </div>
-     `
-     renderPokemonTypes(pokemon) 
+     <h1 id="pokemon-name">${name}</h1>
+     <div class="second-home-info">
+         <p id="home-info"> ${types} </p>
+     </div>
+ </div>
+ <div class="main-container-image">
+     <img src="${img}" id="pokomon-Image">
+ </div>
+ </div>
+     `;
 }
+
+
+function bgColor(i) {
+    let name = allPokemon[i].types[0].type.name;
+    if (name) document.getElementById(`background-color-main(${i})`).classList.add(name);
+
+}
+
 
 // counts the types- 1, 2, 3, 4, 5 ...
 function renderPokemonTypes(pokemon) {
@@ -66,48 +79,108 @@ function closePokemonHTML() {
 }
 
 // return this function in openPokemon
-function openPokemonDetailHTML(pokemonId) {
-    const pokemon = pushedPokemons.find(p => p.id == pokemonId)
+function renderDialog(i) {
+    let name = allPokemon[i][`name`];
+    let types = allPokemon[i][`types`][0][`type`][`name`];
+    let img = allPokemon[i][`sprites`][`other`][`dream_world`][`front_default`];
+    let height = allPokemon[i][`height`];
+    let weight = allPokemon[i][`weight`];
+    document.getElementById(`dialog`).classList.remove(`d-none`);
+    let mainContainer = document.getElementById('dialog');
+    mainContainer.innerHTML = generateRenderDialogHtml(name, img, types, height, weight, i);
+    for (let j = 0; j < 6; j++) {
+        let attack = allPokemon[i][`stats`][j][`stat`][`name`];
+        let nr = allPokemon[i][`stats`][j][`base_stat`];
+        let nrBar = nr / 1.25;
+        let mainContainer = document.getElementById('second-info-dialog');
+        mainContainer.innerHTML += generateSpezInfoHtml(attack, nrBar, nr);
+    }
+    cardColor(i);
+}
 
-    const pokemonName = pokemon['forms'][0]['name']
-    const pokemonImage = pokemon ['sprites']['other']['dream_world']['front_default']
-    const pokemonType = pokemon['types'][0]['type']['name']
-    const pokemonID =  pokemon['id']
-    const pokemonAbility = pokemon['abilities'][0]['ability']['name']
-    const pokemonHeight = pokemon['height']
-    const pokemonWeight = pokemon['weight']
-    const pokemonStat = pokemon['stats'][0]['base_stat']
-    const pokemonAttack = pokemon['stats'][0]['base_stat']
 
+function notClose(event) {
+    event.stopPropagation();
+}
+
+
+function closeDialog() {
+    document.getElementById(`dialog`).classList.add(`d-none`);
+}
+
+function moveRight(i) {
+    if (i < allPokemon.length - 1) {
+        i++
+    } else {
+        i = 0
+    }
+    document.getElementById('dialog').innerHTML = ``;
+    renderDialog(i);
+}
+
+
+function moveLeft(i) {
+    if (i !== 0) {
+        i--
+    } else {
+        i = allPokemon.length - 1
+    }
+    document.getElementById('dialog').innerHTML = ``;
+    renderDialog(i);
+}
+
+
+function generateRenderDialogHtml(name, img, types, height, weight, i) {
     return `
-    <div class="detail">
-    <div class="detailCard ${pokemonType}">
-       
-        <div class="detailButton" >
-            <h2>#${pokemonID}</h2>
-            <button onclick="closePokemonHTML()" class="btn" style="color: #020506;">X</button>
-        </div>
-        <div class="detailName">
-        <h2>
-            ${pokemonName}
-        </h2>
-        </div>
-        <div class="detailImg">
-            <img src="${pokemonImage}">
-        </div>
-        <div class="containerSkills">
-        <div class="skills">
-            <p>Ability: <span> ${pokemonAbility}</span></p>
-            <p>Height: <span> ${pokemonHeight}m</span></p>
-            <p>Weight: <span> ${pokemonWeight}kg</span></p>
-            <p>Base-State: <span> ${pokemonStat}</span> </p>
-            <p>Attack: <span> ${pokemonAttack}</span> </p>
-         </div>
-        </div>
+<div class="container-spez-info" >
+<div class="container-spez-info-width" onclick="notClose(event)" >
+<div class="dialog-info" id="background-color-card(${i})" >
+<h1>${name}</h1>
+</div>
+<div id="second-info-dialog" >
+<div class="image" >
+<img src="img/left.png" id="left-Image" onclick="moveLeft(${i})">
+<img src="${img}" id="pokomon-Image">
+<img src="img/right.png" id="right-Image" onclick="moveRight(${i})">
+</div>
+<div class="spez-over-info-container" >
+<div class="spez-over-info" >
+<span class="margin-bottom-spez" > <b> Typ </b> </span>
+<span>${types}</span>
+</div>
+<div class="spez-over-info" >
+<span class="margin-bottom-spez" > <b> Height</b></span>
+<span>${height} ft</span>
+</div>
+<div class="spez-over-info" >
+<span class="margin-bottom-spez" > <b> Weight</b></span>
+<span>${weight} lbs</span>
+</div>
+</div>
+</div>
+</div>
+</div>
+`;
+}
+
+function generateSpezInfoHtml(attack, nrBar, nr) {
+    return `
+        <div class="spez-info-container">
+    <div class="spez-info" >
+    <div class="attack" > <b> ${attack}</b> </div>
+    <div class="nr-diagram" > 
+    <div class="nr-digram-bar" style="width: ${nrBar}%; background-color: green"> ${nr} </div>
     </div>
-</div>    
+    </div>
     `
 }
+
+function cardColor(i) {
+    let name = allPokemon[i].types[0].type.name;
+    if (name) document.getElementById(`background-color-card(${i})`).classList.add(name)
+
+}
+
 
 // it renders the searched name if its in array 
 function searchPokemon() {
@@ -123,9 +196,3 @@ function searchPokemon() {
 }
 
 
-function renderPokemons(pokemons) {
-    document.getElementById('pokemon-card').innerHTML = '';
-    for (let j = 0; j < pokemons.length; j++) {
-        renderPokemon(pokemons[j])
-    }
-}
